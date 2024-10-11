0ae81fba30679db047cd69851eb2cd2e792ebb50
Ricardo Bonfim <49947316+bonfimr@users.noreply.github.com>
2024-10-11 18:14:25 -0400

    Update devweather.ino

--- firmware/ESP32/devweather.ino
+++ firmware/ESP32/devweather.ino
@@ -1,82 +1,160 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 #include <DHT.h>
 
+#define MODE_MANUAL 0
+#define MODE_AUTO   1
 #define DHTPIN 15       
 #define DHTTYPE DHT11
 
-// Definição dos pinos de LED e LDR
-int ledPin = 25;
-int ldrPin = 36;
-
-int ledValue = 10; // Valor inicial do LED
-int ldrMax = 4000; // Valor máximo do LDR
-
+Adafruit_BMP085 bmp;
 DHT dht(DHTPIN, DHTTYPE);
+int ledPin = 25;
+int ledChannel = 0;
+int ledValue = 10;
+int ledMode = MODE_MANUAL;
+
+int ldrPin = 34;
+int ldrMax = 4000;
+
+int thresholdValue = 50;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    for (int i=9; i>=0; i--) {
+        digitalWrite(LED_BUILTIN, i % 2 ? HIGH : LOW);
+        delay(50);
+    }
+    if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP085/BMP180 sensor, check wiring!");
+    while (1) {}
+    }
+    ledcSetup(1, 5000, 8);
+    ledcAttachPin(LED_BUILTIN, 1); // Led Builtin aceita PWM no ESP32
     
     pinMode(ledPin, OUTPUT);
-    pinMode(ldrPin, INPUT);
-    Serial.printf("DevWeather Initialized.\n");
-
-   processCommand("GET_LDR");
+    ledcSetup(ledChannel, 5000, 8);
+    ledcAttachPin(ledPin, ledChannel);
+    
+    Serial.printf("DBG DevWeather Initialized.\n");
 }
 
-// Função loop será executada infinitamente pelo ESP32
+ 
+
 void loop() {
-  if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n');
-    processCommand(command);
-  }    
+    String serialCommand;
+
+    while (Serial.available() > 0) {
+        char serialChar = Serial.read();
+        serialCommand += serialChar; 
+
+        if (serialChar == '\n') {
+            processCommand(serialCommand);
+            serialCommand = "";
+        }
+    }
+
+    ledUpdate();
+    delay(100);
 }
+
 
 void processCommand(String command) {
-    if (command.startsWith("SET_LED")) {
-        int value = command.substring(8).toInt(); //Pega o valor para  representar a intensidade de luminosidade do led 
-        if (value >= 0 && value <= 100) {
-            ledValue = map(value, 0, 100, 0, 255); //Normaliza o valor da  intensidade do led entre 0 e 255 
+    command.trim();
+    command.toUpperCase();
+
+    // Serial.println("DBG Received command: " + command);
+
+    if (command.startsWith("SET_LED ")) {
+        int ledTmp = command.substring(8).toInt();
+        if (ledTmp >= 0 && ledTmp <= 100) {
+            ledValue = ledTmp;
+            ledMode  = MODE_MANUAL;
             ledUpdate();
-            Serial.print("RES SET_LED "); //Valor inserido está no intervalo 0 a 100
-            Serial.println(value);
-        } else {
-            Serial.println("RES SET_LED -1"); //Entrada inválida do comando SET_LED
+            Serial.printf("RES SET_LED 1\n");
         }
-    } else if (command.startsWith("GET_LED")) {
-        Serial.print("RES GET_LED ");
-        Serial.println(map(ledValue, 0, 255, 0, 100));
-    } else if (command.startsWith("GET_LDR")) {
-        Serial.print("RES GET_LDR ");
-        int ldrValue = ldrGetValue();
-        Serial.println(map(ldrValue, 0, ldrMax, 0, 100));
-    } else if(command.startsWith("GET_TEMP")){
-        Serial.print("RES GET_TEMP ");
-        int tempDHT = getTemp();
-        Serial.println(tempDHT);
-    } else if(command.startsWith("GET_HUM")){
-        Serial.print("RES GET_HUM ");
-        int humDHT = getHum();
-        Serial.println(humDHT);
-    } else {
-        Serial.println("ERR Unknown command.");
+        else {
+            Serial.printf("RES SET_LED -1\n");
+        }
+    }
+    else if (command.startsWith("SET_THRESHOLD ")) {
+        int thresholdTmp = command.substring(14).toInt();
+        if (thresholdTmp >= 0 && thresholdTmp <= 100) {
+            thresholdValue = thresholdTmp;
+            ledMode = MODE_AUTO;
+            ledUpdate();
+            Serial.printf("RES SET_THRESHOLD 1\n");
+        }
+        else {
+            Serial.printf("RES SET_THRESHOLD -1\n");
+        }
+    }
+
+    else if (command == "GET_LDR")
+      Serial.printf("RES GET_LDR %d\n", ldrGetValue());
+
+    else if (command == "GET_LED")
+      Serial.printf("RES GET_LED %d\n", ledValue);
+    
+    else if (command == "GET_THRESHOLD")
+      Serial.printf("RES GET_THRESHOLD %d\n", thresholdValue);
+
+    else if (command == "GET_PA")
+      Serial.printf("RES GET_PA %d\n",  bmpGetPaValue());
+
+    else if (command == "GET_ALT")
+      Serial.printf("RES GET_ALT %d\n", bmpGetAltValue());
+
+    else if (command == "GET_TBMP")
+      Serial.printf("RES GET_TBMP %d\n", bmpGetTempValue());
+
+    else if (command == "GET_TEMP")
+      Serial.printf("RES GET_TEMP %d\n", dhtGetTempValue());
+
+    else if (command == "GET_HUM")
+      Serial.printf("RES GET_HUM %d\n", dhtGetHumValue());
+
+    else if (command == "GET_INFO")
+      Serial.printf("RES GET_INFO %d;%d;%d;%d;%d;%d\n",ldrGetValue(),dhtGetTempValue(), dhtGetHumValue(), bmpGetPaValue(), bmpGetAltValue(), bmpGetTempValue());
+    
+    else
+      Serial.println("ERR Unknown command.");
+      
+}
+void ledUpdate() {
+    if (ledMode == MODE_MANUAL || (ledMode == MODE_AUTO && ldrGetValue() < thresholdValue)) {
+        ledcWrite(ledChannel, 255.0*(ledValue/100.0));
+        ledcWrite(1, 255.0*(ledValue/100.0));
+    }
+    else {
+        ledcWrite(ledChannel, 0);
+        ledcWrite(1, 0);
     }
 }
 
-// Função para ler o valor da umidade
-float getHum(){
-  return dht.readHumidity();
-}
-
-// Função para ler o valor da temperatura
-float getTemp(){
-  return dht.readTemperature();
-}
-
-// Função para atualizar o valor do LED
-void ledUpdate() {
-    analogWrite(ledPin, ledValue);
-}
-
-// Função para ler o valor do LDR
 int ldrGetValue() {
-    return analogRead(ldrPin);
+    int ldrAnalog = analogRead(ldrPin);
+    int ldrValue = 100*ldrAnalog/ldrMax;
+
+    // Serial.printf("DBG LDR_MAX=%d, LDR_ANALOG=%d, LDR_VALUE=%d\n", ldrMax, ldrAnalog, ldrValue);
+
+    return ldrValue > 100 ? 100 : ldrValue;
+}
+
+
+int bmpGetPaValue(){
+    return int(bmp.readPressure());
+}
+int bmpGetAltValue(){
+    return int(bmp.readAltitude());
+}
+int bmpGetTempValue(){
+    return int(bmp.readTemperature());
+}
+int dhtGetTempValue(){
+    return int(dht.readTemperature());
+}
+int dhtGetHumValue(){
+    return int(dht.readHumidity());
 }
